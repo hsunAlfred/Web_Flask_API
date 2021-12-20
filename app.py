2cae235ff9e97e7349ee646df73db14b3eab31fc
hsunAlfred <chenghsungt@gmail.com>
2021-12-20 19:47:09 +0800

    741

--- app.py
+++ app.py
@@ -41,13 +41,12 @@ def hello():
 @app.route('/yolo', methods=['POST', 'GET'])
 def yolo():
     if request.method == 'GET':
-        img_64 = request.args.get('img')
-        print(img_64)
+        img_64 = request.args.get('img').replace('data:image/jpg;base64,', '')
 
-        with open('./yoloSettings/temp.jpg', 'wb') as f:
-            img_data = base64.b64decode(
-                str(img_64).replace('data:image/jpg;base64,', ''))
+        with open('./yoloSettings/temp.jpg', 'w') as f:
+            img_data = base64.b64decode(img_64)
             f.write(img_data)
+
         res = di.main(targetFig="./yoloSettings/temp.jpg")
 
         return jsonify(res[1])
